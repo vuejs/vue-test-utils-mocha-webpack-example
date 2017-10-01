8c8abc29c8f78b50b392004d124e9b82e5a61719
Evan You <yyx990803@gmail.com>
2017-10-01 17:53:25 -0400

    tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -18,12 +18,7 @@ module.exports = {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-          }
-          // other vue-loader options go here
-        }
+        loader: 'vue-loader'
       },
       {
         test: /\.js$/,
