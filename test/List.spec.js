cf27fcfe7a7c6b3fbf170cf97aa05b08867875b8
eiji.ienaga <e-ienaga@esm.co.jp>
2018-06-18 21:01:28 +0900

    fix: change shallow -> shallowMount (#9)

--- test/List.spec.js
+++ test/List.spec.js
@@ -1,10 +1,10 @@
-import { shallow } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import List from '@/components/List.vue'
 
 describe('List.vue', () => {
   it('renders li for each item in props.items', () => {
     const items = ['', '']
-    const wrapper = shallow(List, {
+    const wrapper = shallowMount(List, {
       propsData: { items }
     })
     expect(wrapper.findAll('li')).toHaveLength(items.length)
